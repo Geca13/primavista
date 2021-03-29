150898154bf8ec882c589698ff8dec8bd97bcf4d
stole@primavista.mk <stole@primavista.mk>
2021-03-29 14:29:15 +0200

    commit

--- src/main/java/com/example/primavista/documents/entity/User.java
+++ src/main/java/com/example/primavista/documents/entity/User.java
@@ -11,7 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
 
 import org.hibernate.annotations.NaturalId;
@@ -26,6 +25,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Integer id;
